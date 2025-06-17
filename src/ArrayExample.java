0da4d1a49ed890e9552aa80466e0671024009552
Timmy20s <190116654+Timmy20s@users.noreply.github.com>
2025-06-17 21:54:13 +0100

    Update ArrayExample.java

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -1,21 +1,40 @@
+package basics;
+
 import java.util.Scanner;
 
-public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+public class ArrayExample {
 
-    int[] myArray = {1,3,5,7,9};
-    int[] anotherArray = new int [10];
-    String[] gender = {"Female" ,  "Male"};
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] myArray = {1, 3, 5, 7, 9};
+        //or
+        int[] anotherArray = new int[10];
+        int sum = 0;
+        String[] gender = {"Female", "Male"};
+        //would prefer to focus on for loops over while loops
+        for (int i= 0; i< myArray.length; i++){
+            System.out.println("Item at: " +i+ " is " + myArray[i]);
 
-    for (int i = 0; i < myArray.lenght; i++) {
-        if (anotherArray[i] ==) {
-            System.out.println("Insert Item at: "+i+);
+            //to update the value of a variable eg.3
+            myArray[1] = 13;
+            anotherArray[5] = 17;
+
+        for (int k = 0; k < anotherArray.length; k++) {
+            System.out.println("Insert item at: " + k);
+            anotherArray[k] = input.nextInt();
+            System.out.println("Item at: " +k+ " is " + anotherArray[k]);
+
+            //to look for an item, search
+        for( int i = 0; i < anotherArray.length; i++){
+            if (anotherArray[i] ==13);
+            System.out.println("Found it");
+            break;
+
+            //use a for loop to get the sum of items within myArray
+        for(int j=0; j< myArray.length; j++){
+            sum += myArray[j];
+            System.out.println("Sum = " + sum);
         }
-
-int sum = 0;
-        for (int i = 0; i < myArray.lenght; i++) {
-            sum += myArray[i];
-            System.out.print("sum");
         }
-}
-}
+        }
+        }
